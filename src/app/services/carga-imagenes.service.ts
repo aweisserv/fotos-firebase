c2eda9e4e8fd88cc619d7abb6d9d5ec0afe0f9c1
Allan Weisser Verdessi <vredes@live.com>
2021-08-10 23:18:51 -0400

    load files ui with functional service button

--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import * as firebase from 'firebase';
+import { FileItem } from '../models/file-item';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,12 @@ export class CargaImagenesService {
   private CARPETA_IMAGENES = 'img';
 
   constructor( private db: AngularFirestore ) {  }
+
+  cargarImagenesFirebase( imagenes: FileItem[] ) {
+    
+    console.log( imagenes )
+
+  }
 
   private guardarImagen( imagen: { nombre: string, url: string } ) {
     
