cc3a5176e99e921fed63514bf106d5f560dbb2e3
Allan Weisser Verdessi <vredes@live.com>
2021-08-10 21:43:18 -0400

    angular fire and service implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,14 @@ import { FotosComponent } from './components/fotos/fotos.component';
 import { CargaComponent } from './components/carga/carga.component';
 import { APP_ROUTES } from './app.route';
 
+//Firebsae
+import { CargaImagenesService } from './services/carga-imagenes.service';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore'
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +22,14 @@ import { APP_ROUTES } from './app.route';
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [
+    CargaImagenesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    
+    production: false,  
+    firebase: {  
+    apiKey: "AIzaSyBBr88N5vGPNj5A51YcfYg1WXtb-fITP_o",
+    authDomain: "fir-fotos-99d51.firebaseapp.com",
+    databaseURL: "https://fir-fotos-99d51-default-rtdb.firebaseio.com",
+    projectId: "fir-fotos-99d51",
+    storageBucket: "fir-fotos-99d51.appspot.com",
+    messagingSenderId: "1069284309098",
+    appId: "1:1069284309098:web:b91b194f2043bf3570f213",
+    measurementId: "G-FRX8BWL0W4"
+  }
+
 };
 
 /*
