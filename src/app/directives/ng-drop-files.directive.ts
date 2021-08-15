c945f3db7874ef32db05e0a871180b6c5df32f9e
Allan Weisser Verdessi <vredes@live.com>
2021-08-14 21:41:27 -0400

    added directives

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -1,10 +1,12 @@
 import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from '@angular/core';
+import { FileItem } from '../models/file-item';
 
 @Directive({
   selector: '[appNgDropFiles]'
 })
 export class NgDropFilesDirective {
 
+  @Input() archivos: FileItem[] = [];
   @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
 
   constructor() { }
@@ -17,6 +19,36 @@ export class NgDropFilesDirective {
   @HostListener('dragleave', ['$event'])
   public onDragLeave( event: any ) {
     this.mouseSobre.emit( false );
+  }
+
+
+  //Validaciones
+  private _archivoListo( archivo: File ): boolean {
+    if ( !this._archivoDropeado( archivo.name ) && this._esImagen( archivo.type )) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private _prevenirDetener( event: any ) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  private _archivoDropeado( nombreArchivo: string ): boolean {
+    
+    for ( const archivo of this.archivos ) {
+      if ( archivo.nombreArchivo == nombreArchivo ) {
+        console.log("El archivo " + nombreArchivo + "ya está agregado");
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private _esImagen( tipoArchivo: string ): boolean {
+    return ( tipoArchivo == '' || tipoArchivo === undefined ) ? false : tipoArchivo.startsWith('image');
   }
 
 }
