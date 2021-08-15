617f279d84c0425f56414b51730b1e6a1f8d99f4
Allan Weisser Verdessi <vredes@live.com>
2021-08-14 22:55:23 -0400

    validators working

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -14,11 +14,49 @@ export class NgDropFilesDirective {
   @HostListener('dragover', ['$event'])
   public onDragEnter( event: any ) {
     this.mouseSobre.emit( true );
+    this._prevenirDetener( event );
   }
 
   @HostListener('dragleave', ['$event'])
   public onDragLeave( event: any ) {
     this.mouseSobre.emit( false );
+  }
+
+  @HostListener('drop', ['$event'])
+  public onDrop( event: any ) {
+
+    const transferencia = this._getTransferencia( event );
+
+    if( !transferencia ){
+      return;
+    }
+
+    this._extraerArchivos( transferencia.files );
+
+    this._prevenirDetener( event );
+
+    this.mouseSobre.emit( false );
+
+  }
+
+  private _getTransferencia( event: any ) {
+    return event.dataTransfer ? event.dataTransfer : event.originalEvent.dataTransfer;
+  }
+
+  private _extraerArchivos( archivosLista: FileList ) {
+    for ( const propiedad in Object.getOwnPropertyNames( archivosLista ) ){
+
+      const archivoTemporal = archivosLista[propiedad];
+
+      if ( this._archivoListo( archivoTemporal ) ){
+
+        const nuevoArchivo = new FileItem( archivoTemporal );
+        this.archivos.push( nuevoArchivo );
+
+      }
+    }
+
+    console.log( "Datos: ", this.archivos );
   }
 
 
