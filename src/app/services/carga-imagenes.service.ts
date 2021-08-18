5ef6f1873b7cf7c2e4f2d5d32a5e6f7d58bb7018
Allan Weisser Verdessi <vredes@live.com>
2021-08-17 23:48:38 -0400

    firestore fixed and updated

--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import * as firebase from 'firebase';
+import firebase from 'firebase/app';
 import { FileItem } from '../models/file-item';
 
 @Injectable({
@@ -14,15 +14,12 @@ export class CargaImagenesService {
 
   cargarImagenesFirebase( imagenes: FileItem[] ) {
     
-    console.log( imagenes )
+    const storageRef = firebase.storage().ref(); //comandos de firebase -> ver doc
 
   }
 
   private guardarImagen( imagen: { nombre: string, url: string } ) {
-    
-    this.db.collection(`/${ this.CARPETA_IMAGENES }`)
-            .add( imagen );
-
+    this.db.collection(`/${ this.CARPETA_IMAGENES }`).add( imagen );
   }
 
 }
