f2a1cc94b242261544054bb0d5b10f75f9bc95ae
Allan Weisser Verdessi <vredes@live.com>
2021-08-18 23:58:55 -0400

    show images alpha

--- src/app/components/fotos/fotos.component.ts
+++ src/app/components/fotos/fotos.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Item { name: string; url: any; }
 
 @Component({
   selector: 'app-fotos',
   templateUrl: './fotos.component.html'
 })
 export class FotosComponent implements OnInit {
+
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
 
   constructor() { }
 
