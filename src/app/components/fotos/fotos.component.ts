bcff855bfe21ae3fd68b03e64b68a3695c6c35bb
Allan Weisser Verdessi <vredes@live.com>
2021-08-20 00:52:07 -0400

    view photos in firestore database section working

--- src/app/components/fotos/fotos.component.ts
+++ src/app/components/fotos/fotos.component.ts
@@ -2,25 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
-export interface Item { nombre: string; url: any; }
+export interface Item { nombre: string, url: string; }
 
 @Component({
   selector: 'app-fotos',
   templateUrl: './fotos.component.html'
+
 })
-export class FotosComponent implements OnInit {
+export class FotosComponent {
 
   private itemsCollection: AngularFirestoreCollection<Item>;
   public items: Observable<Item[]>;
 
   constructor( private afs: AngularFirestore ) {
-
-    this.itemsCollection = afs.collection<Item>('items');
+    
+    this.itemsCollection = afs.collection<Item>('img');
     this.items = this.itemsCollection.valueChanges();
 
    }
 
-  ngOnInit(): void {
+   addItem(item: Item) {
+    this.itemsCollection.add(item);
   }
+
 
 }
